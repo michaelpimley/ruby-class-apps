2f2989c07288a2bc55870e0abf8825477be29e3f
michaelpimley <michaelpimley@gmail.com>
2016-09-27 17:13:55 +0000

    initialized 200 w/ faker

--- users-listings/db/seeds.rb
+++ users-listings/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+200.times do |i|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  email = Faker::Internet.email
+  title = Faker::Name.title
+  User.create(first_name: first_name, last_name: last_name, email: email, position: title)
+end
